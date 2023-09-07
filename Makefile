8a4dbb744285de1eec6357868ac4e5eea8e075e1
Jimmy <jaymeet5087d@gmail.com>
2023-09-07 12:57:35 -0500

    updated

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
+SRC = main.cpp
+
 all: Homework1.o
 	g++ -g Homework1.o -o Homework1
 
-Homework1.o: Homework1.cpp
-	g++ -g Homework1.cpp -c -o Homework1.o
+Homework1.o: ${SRC}
+	g++ -g ${SRC} -c -o Homework1.o
 
 clean:
 	rm -rf Homework1.o Homework1
--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 //
 // Homework 1
-// CS 341 Spring 2021
-// << Your Name Here >>
+// CS 341 Fall 2023
+// Jimmy Patel
 //
 
-int main()
-{
 
-	return 0;
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string filename;
+    getline(cin, filename); // reading line into filename from input
+    string line;
+
+    ifstream myfile(filename); //opening file for reading only
+
+    if(!myfile.is_open()){
+        cerr << "Unable to locate the file" << endl; // display error message if file is not open or found
+    }else{
+        while (!myfile.eof()){ // loop until file reaches end of file
+            getline(myfile,line); // reading contents of file line by line
+            cout << line << endl; // printing each line
+        }
+    }
+    myfile.close();
+
+    return 0;
 }
